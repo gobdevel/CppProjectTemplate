f194f5d7ffce9e12e1e4cd55630041fdbb0d1e5f
gobdevel <gobdeveloper@gmail.com>
2022-07-29 20:40:34 +0530

    some general changes

--- app/main.cpp
+++ app/main.cpp
@@ -6,10 +6,10 @@ int main()
 {
   Infra::Config::getInstance().init("filename");
 
-  // If you want to initialise file logging
+  /* If you want to initialise file logging */
   // Infra::Logger::init("mainApp", "/tmp/testLog.log");
 
-  // Enable console logging
+  /* Enable console logging */
   Infra::Logger::init();
 
   App app;
